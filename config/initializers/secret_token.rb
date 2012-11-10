e25dd52e910540c30b5875ba3d9ef0b378c9f0e6
Renée De Voursney <renee@nird.us>
2012-11-10 10:07:17 -0800

    bare rails app for new nird site

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NirdUs::Application.config.secret_token = 'f5a74745dd2d0043429024417d100fc07c4896ac6d66bb1d49b906ac1693a09e54f45c5d9cdf3e3824af7050526289ba62405ecded5877ef3ca7ce82784be7a1'
